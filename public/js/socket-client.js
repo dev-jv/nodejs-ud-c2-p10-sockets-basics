684bfa536cd3f49080eab1825832df5e9d34bb8f
dev-jv <dev.javierleon@gmail.com>
2021-02-16 11:22:35 -0500

    four

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,18 @@
+
+// HTML reference
+const lblOnline = document.querySelector('#lblOnline');
+const lblOfline = document.querySelector('#lblOffline');
+
 const socket = io();
+
+socket.on('connect', () => {
+    console.log('Connected');
+    lblOnline.style.display = '';
+    lblOfline.style.display = 'none';
+});
+
+socket.on('disconnect', () => {
+    console.log('Disconnected');
+    lblOnline.style.display = 'none';
+    lblOfline.style.display = '';
+});
